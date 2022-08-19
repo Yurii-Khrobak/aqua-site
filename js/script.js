f35542b2999a0196d310b4a61d85e625acd3cc6e
Yurii-Khrobak <khrobakyuriuwork@gmail.com>
2022-08-19 11:27:38 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.comments-slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		centerMode: true,
+		infinity: false
+	})
+})
